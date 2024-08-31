faed1a973aa8738a4034ee9276ccb3342170699c
keerthiv <keerthi@sparkouttech.com>
2024-08-31 10:16:54 +0530

    Initial Commit

---
+++ Sources/MyLibrary/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by apple on 31/08/24.
+//
+
+import SwiftUI
+
+public struct SwiftUIView: View {
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//#Preview {
+//    SwiftUIView()
+//}
