e65664bfa753e3cde84da90c97d472e27a3720f1
keerthiv <keerthi@sparkouttech.com>
2024-08-31 11:02:18 +0530

    viewmodel added

--- Sources/MyLibrary/ViewModel.swift
+++ Sources/MyLibrary/ViewModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import Combine
+import Alamofire
+
+struct User {
+    var firsName: String
+}
+
+protocol ApiProtocol {
+    func fetchData(url: String) -> AnyPublisher<User, AFError>
+}
+
+class NetworkService: ApiProtocol {
+    func fetchData(url: String) -> AnyPublisher<User, AFError> {
+        guard let url = URL(string: url), let request = URLRequest(url: url) else {
+            return
+            
+        }
+        
+        AF.request(request, method: .get)
+            .validate()
+            .publishDecodable(type: User.self)
+            .value()
+            .receive(on: DispatchQueue.main)
+    }
+}
+
+@available(iOS 13.0, *)
+public class ViewModel: ObservableObject  {
+    
+    var delegate: ApiProtocol
+    var anyCancellable: Set<AnyCancellable> = .init()
+    
+    public init(delegate: ApiProtocol) {
+        self.delegate = delegate
+    }
+    
+    public func fetchDetails() -> AnyPublisher<User, AFError> {
+        delegate?.fetchData(url: "https://ipapi.co/json/")
+            .map { response in
+                return response
+            }.eraseToAnyPublisher()
+    }
+    
+}
